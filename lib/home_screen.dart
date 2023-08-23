e00af79970282a9888790411de0fa1f358cb068a
maheshprasad <mahesh.prasad@ideas2tech.com>
2023-08-23 12:30:50 +0530

    GetX Change Light and Dark Theme

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -16,7 +16,61 @@ class _HomeScreenState extends State<HomeScreen> {
         title: Text("GetX Tutorial"),
       ),
       body: Column(
-        children: [],
+        children: [
+          Card(
+            child: ListTile(
+              title: const Text("GetX Dialog Alert"),
+              subtitle: const Text("GetX dialog alert with getX"),
+              onTap: () {
+                Get.defaultDialog(
+                    title: "Delete Chat",
+                    titlePadding: const EdgeInsets.only(top: 20),
+                    contentPadding: const EdgeInsets.all(20),
+                    middleText: "Are you sure you want to delete this chat",
+                    confirm: TextButton(
+                        onPressed: () {
+                          Get.back();
+                        },
+                        child: const Text('Ok')),
+                    cancel: TextButton(
+                        onPressed: () {}, child: const Text('Cancel')));
+              },
+            ),
+          ),
+          Card(
+            child: ListTile(
+              title: const Text("GetX Bottom Sheet"),
+              subtitle: const Text("GetX dialog alert with getX"),
+              onTap: () {
+                Get.bottomSheet(
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.red,
+                      borderRadius: BorderRadius.circular(10)
+                    ),
+                    child: Column(
+                      children: [
+                        ListTile(leading: const Icon(Icons.light_mode),
+                          title: const Text('Light Theme'),
+                          onTap: (){
+                              Get.changeTheme(ThemeData.light());
+                          },
+                        ),
+
+                        ListTile(leading: const Icon(Icons.dark_mode),
+                          title: const Text('Dark Theme'),
+                          onTap: (){
+                            Get.changeTheme(ThemeData.dark());
+                          },
+                        ),
+                      ],
+                    ),
+                  )
+                );
+              },
+            ),
+          )
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
